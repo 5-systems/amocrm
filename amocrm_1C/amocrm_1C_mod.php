5238b5c601467cd972e48f4d7584abf1e9c4ff7d
a.titov <a.titov@localhost.localdomain>
2018-05-04 14:08:06 +0300

    Добавлено расширение интеграции с 1С

---
+++ amocrm_1C/amocrm_1C_mod.php
@@ -0,0 +1,21 @@
+<?php 
+
+
+function amocrm_1C_create_contact(&$http_requester, $contact_data, $user_id, $contacts_array=array(), $companies_array=array(), &$error_status=false, $LogLineId='') {
+   
+   $result=array();
+   
+   
+   
+   
+   return($result);   
+}
+
+
+
+
+
+
+
+
+?>
